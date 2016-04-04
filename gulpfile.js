616304a16a0c24777db634d0b5cdecd37fa2a62c
Vadim Makeev <pepelsbey@gmail.com>
2016-04-04 19:27:40 +0300

    PostCSS-based styles workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const beml = require('gulp-beml');
-const csso = require('gulp-csso');
+const csso = require('postcss-csso');
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
+const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 const sync = require('browser-sync').create();
 
 gulp.task('styles', () => {
 	return gulp.src('src/styles/screen.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer())
-		.pipe(csso())
+		.pipe(postcss([
+			autoprefixer,
+			csso
+		]))
 		.pipe(gulp.dest('dest/styles'))
 		.pipe(sync.stream());
 });
