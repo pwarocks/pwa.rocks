16bbd8d1525614de2a159b76cbb608cbce977dd6
Kornel Lesiński <kornel.lesinski@ft.com>
2016-07-22 15:52:08 +0100

    Financial Times

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -49,6 +49,7 @@ self.addEventListener('install', function(event) {
 				'/images/svgomg.svg',
 				'/images/telegram.svg',
 				'/images/voice-memos.svg',
+				'/images/financial-times.svg',
 				'/images/washington-post.svg',
 				'/images/wave-pd1.svg',
 				'/images/webnfc.svg',
