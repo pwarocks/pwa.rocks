00bb0876352a8dc87e7d9acc7bb1d26fdb55f659
Vadim Makeev <pepelsbey@gmail.com>
2016-10-17 08:47:31 +0300

    Assets moved up

---
+++ src/script.js
@@ -0,0 +1,70 @@
+(function() {
+
+	// Elements
+
+	var tags = document.querySelectorAll('.js-tag');
+	var list = document.querySelector('.js-list');
+	var apps = document.querySelectorAll('.js-app');
+
+	// Storage. Courtesy of Mathias Bynens
+	// https://mathiasbynens.be/notes/localstorage-pattern
+
+	var storage = (function() {
+		var uid = new Date;
+		var storage;
+		var result;
+		try {
+			(storage = window.sessionStorage).setItem(uid, uid);
+			result = storage.getItem(uid) == uid;
+			storage.removeItem(uid);
+			return result && storage;
+		} catch (exception) {}
+	}());
+
+	// Shuffle. Courtesy of Dudley Storey
+	// https://thenewcode.com/82/Recipes-for-Randomness-in-JavaScript
+
+	var shuffle = (function (array) {
+		for (var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x);
+		return array;
+	});
+
+	// Tags
+
+	tags[0].checked = true;
+
+	for (var i = 0; i < tags.length; i++) {
+		tags[i].addEventListener('change', function() {
+			list.dataset.filter = this.value;
+		});
+	}
+
+	// Reorder
+
+	function getOrder(list) {
+		var list = list.children;
+		var order = [];
+		for (var i = 0; i < list.length; i++) {
+			order.push(list[i].dataset.app);
+		}
+
+		return order;
+	}
+
+	function setOrder(order) {
+		if (storage) {
+			var item = storage.getItem('pwa-list-order');
+			order = item ? item.split(',') : order;
+		}
+
+		for (var i = 0; i < list.children.length; i++) {
+			var item = list.querySelector('[data-app=' + order[i] + ']')
+			list.appendChild(item);
+		}
+
+		storage.setItem('pwa-list-order', order.join());
+	}
+
+	setOrder(shuffle(getOrder(list)));
+
+}());
